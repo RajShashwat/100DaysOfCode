04e684e67b8e24a79680ba28875cceb853111bdc
Shashwat Raj <shashwat.r2018bcs@srisriuniversity.edu.in>
2020-09-10 23:44:19 +0530

    Create RemoveDuplicate.py

---
+++ Day 4/RemoveDuplicate.py
@@ -0,0 +1,14 @@
+def removeDuplicates(nums: [int]) -> int:
+        i = len(nums)
+        while i > 1:
+            i -= 1
+            if nums[i] == nums[i - 1]:
+                del nums[i]
+        return len(nums)
+
+nums = list(map(int,input("Enter the array: ").strip().split()))
+len = removeDuplicates(nums)
+print("length of array: ", len)
+print("Array Elements are: ", end="")
+for i in nums:
+    print(i, end=" ")
